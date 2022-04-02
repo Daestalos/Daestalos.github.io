d9225f3cccae7e32fbf4e68b3508b4401ff1333a
Daestalos <alex.bordyug.daes@gmail.com>
2022-04-03 00:03:26 +0300

    feat: add calculator function

--- Project v3 Jquery/src/js/script.js
+++ Project v3 Jquery/src/js/script.js
@@ -1,91 +1,43 @@
 "use strict";
 
-/* Задание:
-1) вопросы на промпт
-2) тип сайта (Сайт-визитка, корпоративный сайт, интернет-магазин)
-3) дизайн (уникальный дизайн, шаблонный дизайн, wow-дизайн)
-4) верстка (Адаптивная / Фиксированная)
-5) цена для вариантов
-6) Объект калькулятор, внутри метод для рассчета цены
-7) алертом выводится стоимость и сроки
-*/
+$(document).ready(function(){
 
-let typeOfSite,
-    typeOfDesign,
-    typeOfLayout;
-
-function rememberTypeOfSite() {
-    typeOfSite = +prompt('Укажите тип сайта (1 - Сайт-визитка, 2 - корпоративный сайт, 3 - интернет-мазагин', 'Например: 1');
-    while (typeOfSite == '' || typeOfSite == null || isNaN(typeOfSite) || typeOfSite > 3) {
-        typeOfSite = +prompt('Укажите тип сайта (1 - Сайт-визитка, 2 - корпоративный сайт, 3 - интернет-мазагин', 'Например: 1');
-    }
-};
-
-function rememberTypeOfDesign() {
-    typeOfDesign = +prompt('Выберите дизайн (1 - уникальный дизайн, 2 - шаблонный дизайн, 3 - wow-дизайн', 'Например: 1');
-    while (typeOfDesign == '' || typeOfDesign == null || isNaN(typeOfDesign) || typeOfDesign > 3) {
-        typeOfDesign = +prompt('Выберите дизайн (1 - уникальный дизайн, 2 - шаблонный дизайн, 3 - wow-дизайн', 'Например: 1');
-    }
-};
-
-function rememberTypeOfLayout() {
-    typeOfLayout = +prompt('Выберите верстку (1 - Адаптивная, 2 - Фиксированная)', 'Например: 1');
-    while (typeOfLayout == '' || typeOfLayout == null || isNaN(typeOfLayout) || typeOfLayout > 2) {
-        typeOfLayout = +prompt('Выберите верстку (1 - Адаптивная, 2 - Фиксированная)', 'Например: 1');
-    }
-};
-
-
-rememberTypeOfSite();
-rememberTypeOfDesign();
-rememberTypeOfLayout();
-
-
-const calculator = {
-    site: typeOfSite,
-    design: typeOfDesign,
-    layout: typeOfLayout,
-    calculatePrise(){
-        let Price = 0,
+    // Калькулятор цены
+    $('#PriceCalculator').click( () => {
+        let TypeSite =  $('#TypeSite').val(),
+            Design = $('#Design').val(),
+            Adaptive = $('#Adaptive').val(),
+            Price = 0,
             Time = 0;
 
-        // site
-        if (this.site === 1 || this.site === 2){
-            Price += 500;
-            Time += 2;
-        }
-        else{
-            Price += 1000;
-            Time += 4;
-        }
+            if (TypeSite == 1 || TypeSite == 2){
+                Price += 500;
+                Time += 2;
+            } else{
+                Price += 1000;
+                Time += 4;
+            }
+            if (Design == 1 ) {
+                Price += 1000;
+                Time += 3;
+            } else if (Design == 2 ) {
+                Price += 500;
+                Time += 1;
+            } else {
+                console.log(Design);
+                Price += 3000;
+                Time += 5;
+            }
+            if(Adaptive == 1){
+                Price += 4000;
+                Time += 1;
+            } else {
+                Price += 4000;
+                Time += 2;
+            }
+              
+        $('#Time').html(`Количество дней выполнения: ${Time} дней`);
+        $('#Price').html(`Стоимость заказа состовляет: ${Price} рублей`);
+    });
 
-        // design
-
-        if (this.design === 1 ) {
-            Price += 1000;
-            Time += 3;
-        }else if (this.design === 2 ) {
-            Price += 500;
-            Time += 1;
-        }else {
-            Price += 3000;
-            Time += 5;
-        }
-
-        // layout
-
-        if(this.layout === 1){
-            Price += 4000;
-            Time += 1;
-        } else {
-            Price += 4000;
-            Time += 2;
-        }
-
-        alert(`Стоимость заказа состовляет: ${Price} рублей, Количество дней выполнения: ${Time}`);
-    }
-};
-
-
-calculator.calculatePrise();
-
+});
