1063b2c0da5372bd1084f67403543bf458182044
Daestalos <alex.bordyug.daes@gmail.com>
2022-04-03 20:20:30 +0300

    feat: new functions

--- Project v3 Jquery/src/js/script.js
+++ Project v3 Jquery/src/js/script.js
@@ -2,25 +2,85 @@
 
 $(document).ready(function(){
 
+    /* magnificPopup */
 
+    $(document).ready(function() {
+        $('.image-link').magnificPopup({type:'image'});
+      });
 
+    /* Модальное окно */
 
+    let modalInterval = setInterval(function () {
+        $("#myModal").modal('show');
+        clearInterval(modalInterval);
+    }, 30000);
+    
+    $('#ModalNotReady').click(function(){
+        $("#myModal").modal('hide');
+        
+    });
+    $('#ModalReady').click(function(){
+        $("#myModal").modal('hide');
+        $('html, body').animate({scrollTop: $('#Price').offset().top - 70 + "px"});
+    });
 
-    // отложенная анимация
-    let options = {threshold: [0.5]};
-    // создаем observer
-    let observer = new IntersectionObserver(onEnrtry, options);
-    // создадим еще одну переменную
-    // к примеру, возьмем, что для всех наших анимаций, для всех элементов, которые мы анимируем зададим класс  .element-animation, соответственно для всех элементов с классом .element-animation у нас будет срабатывать данный скрипт
-    let elements = $('.element-animation');
-    elements.each((i, el) => {
+    /* Якорь */
+
+    $('a[href^="#').click(function(){
+        let valHref = $(this).attr("href");
+        $('html, body').animate({scrollTop: $(valHref).offset().top - 50 + "px"});
+    });
+
+    $(window).scroll(() => {
+        let scrollDistance = $(window).scrollTop();
+        $(".section").each((i, el) => {
+            if($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+                // пробегаемся по всем меню a
+                $("nav a").each((e, el) => {
+                    // убираем у всех активный класс
+                    if ($(el).hasClass("active")){
+                        $(el).removeClass("active");
+                    }
+                });
+                // внутри меню находим элементы li, затем тег a и добавляем ему Active
+                $('nav li:eq('+ i +')').find('a').addClass('active');
+    
+    
+            }
+        });
+    });
+    
+
+    /* Анимация */
+    let options = {threshold: [0.2]};
+    let observer = new IntersectionObserver(onEntry, options);
+
+    // About text animation
+    let AboutTextAnimation = $('.About__Text-animanion');
+    AboutTextAnimation.each((i, el) => {
+        observer.observe(el);
+    });
+
+    // About img animation
+    let AboutImgAnimation = $('.About__Img-Animation');
+    AboutImgAnimation.each((i, el) => {
+        observer.observe(el);
+    });
+
+    // Skills animation
+    let SkillsAnimation = $('.Skills__List');
+    SkillsAnimation.each((i, el) => {
         observer.observe(el);
     });
 
 
-
-
-
+    // Statistics animation
+    let StatisticsAnimation = $('.Statistics__List'),
+        Statisticsobserver = new IntersectionObserver(onEntryStatistic, {threshold: [0.5]});
+    StatisticsAnimation.each((i, el) => {
+        Statisticsobserver.observe(el);
+    });
+    
 
 
     // Калькулятор цены
@@ -57,18 +117,43 @@ $(document).ready(function(){
                 Time += 2;
             }
               
-        $('#Time').html(`Количество дней выполнения: ${Time} дней`);
-        $('#Price').html(`Стоимость заказа состовляет: ${Price} рублей`);
+        $('#TimeValue').html(`Количество дней выполнения: ${Time} дней`);
+        $('#PriceValue').html(`Стоимость заказа состовляет: ${Price} рублей`);
     });
 
 });
 
-function onEnrtry (entry){
-    //
+function onEntry (entry){
     entry.forEach(change => {
-        // если change (наш элемент) попал в наблюдатель, т.е. сработал threshold
-        if (change.isIntersecting){}
-        // то для нашего change, т.е. для нашего элемента добавляем класс show-animation
-        change.target.classList.add('.show-animation');
+        if (change.isIntersecting){ 
+        change.target.classList.add('show-animation');
+    }
+    });
+}
+
+function onEntryStatistic (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){ 
+        change.target.classList.add('show-animation');
+        StatisticsCounter();
+    }
+    });
+}
+
+function StatisticsCounter(){
+    $('.Statistics__List__Subtitle').each(function(){
+        let Statistics = $(this),
+            countEnd = Statistics.attr('data-count');
+        
+        $({ countStart: Statistics.text()}).animate( { countStart: countEnd }, {
+        duration: 3500,
+        easing:'linear',
+        step: function() {
+            Statistics.text(Math.floor(this.countStart));
+        },
+        complete: function() {
+            Statistics.text(this.countStart);
+        }
+        });    
     });
 }
